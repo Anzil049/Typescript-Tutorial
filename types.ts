e33424b254864a2b97476aa0ecc37e03ff4aec7d
anzil049 <anzil049@gmail.com>
2026-02-02 12:44:13 +0530

    Fix variable name typo and add console logs for user details

--- types.ts
+++ types.ts
@@ -1,5 +1,11 @@
-let Username:string="Anzil";
+let userName:string="Anzil";
 let age:number=21;
 let isDeveloper:boolean=true;
 let skills:string[]=["HTML","CSS","JS","Node","Express","MongoDB","React","Typescript"];
 let user:[string,number]=["Anzil",21];
+
+console.log(userName);
+console.log(age);
+console.log(isDeveloper);
+console.log(skills);
+console.log(user);
