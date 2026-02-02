eb3528a260dbf9d873bcbbef82c7cb2c2d31b66e
anzil049 <anzil049@gmail.com>
2026-02-02 12:42:13 +0530

    Add initial TypeScript variable declarations for user profile

---
+++ types.ts
@@ -0,0 +1,5 @@
+let Username:string="Anzil";
+let age:number=21;
+let isDeveloper:boolean=true;
+let skills:string[]=["HTML","CSS","JS","Node","Express","MongoDB","React","Typescript"];
+let user:[string,number]=["Anzil",21];
