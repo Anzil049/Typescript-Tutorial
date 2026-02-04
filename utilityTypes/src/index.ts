891675aff4e46bfadef1520d05a1d2ab89d10a06
anzil049 <anzil049@gmail.com>
2026-02-04 17:49:04 +0530

    utility types practical

--- utilityTypes/src/index.ts
+++ utilityTypes/src/index.ts
@@ -1,18 +1,18 @@
-interface User{
-    id:number;
-    name:string;
-    email:string;
-    password:string;
-    isAdmin:boolean;
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    isAdmin: boolean;
 }
 
 
 //Partial
 
-type updateUser=Partial<User>;
-let userUpdate:updateUser={
-    name:"Anzil",
-    email:"anzil049@gmail.com"
+type updateUser = Partial<User>;
+let userUpdate: updateUser = {
+    name: "Anzil",
+    email: "anzil049@gmail.com"
 }
 
 console.log(userUpdate);
@@ -20,23 +20,23 @@ console.log(userUpdate);
 
 //Pick
 
-type userProfile=Pick<User,"id"|"name"|"email">
-let  profileUser:userProfile={
-    id:100,
-    name:"Anzil",
-    email:"anzil049@gmail.com"
+type userProfile = Pick<User, "id" | "name" | "email">
+let profileUser: userProfile = {
+    id: 100,
+    name: "Anzil",
+    email: "anzil049@gmail.com"
 }
 
 console.log(profileUser);
 
 
 //Omit
-type safeUser=Omit<User,"password">
-let userSafe:safeUser={
-    id:200,
-    name:"Anzil",
-    email:"anzil049@gmail.com",
-    isAdmin:true,
+type safeUser = Omit<User, "password">
+let userSafe: safeUser = {
+    id: 200,
+    name: "Anzil",
+    email: "anzil049@gmail.com",
+    isAdmin: true,
 }
 
 console.log(userSafe);
