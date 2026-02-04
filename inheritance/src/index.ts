6e0928feafdf8fee1c38f6d6bd2f45e23539f40b
anzil049 <anzil049@gmail.com>
2026-02-04 17:47:09 +0530

    class inheritance practical

--- inheritance/src/index.ts
+++ inheritance/src/index.ts
@@ -1,30 +1,30 @@
-class Person{
-    name:string;
-    age:number;
+class Person {
+    name: string;
+    age: number;
 
-    constructor(name:string,age:number){
-        this.name=name;
-        this.age=age;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
     }
 
-    introduce():void{
+    introduce(): void {
         console.log(`My name is ${this.name},I am ${this.age} years old`);
     }
 }
 
-class Teacher extends Person{
-    subject:string;
+class Teacher extends Person {
+    subject: string;
 
-    constructor(name:string,age:number,subject:string){
-        super(name,age);
-        this.subject=subject;
+    constructor(name: string, age: number, subject: string) {
+        super(name, age);
+        this.subject = subject;
     }
-    teach():void{
+    teach(): void {
         console.log(`${this.name} teaches ${this.subject}`);
     }
 }
 
-const teacher1=new Teacher("Anzil",21,"MERN");
+const teacher1 = new Teacher("Anzil", 21, "MERN");
 
 teacher1.introduce();
 teacher1.teach();
