cedade05141861c56e09c08826795d1c214e59b6
anzil049 <anzil049@gmail.com>
2026-02-04 14:53:02 +0530

    omit,pick,partial practice

--- utilityTypes/dist/index.js
+++ utilityTypes/dist/index.js
@@ -4,3 +4,16 @@ let userUpdate = {
     email: "anzil049@gmail.com"
 };
 console.log(userUpdate);
+let profileUser = {
+    id: 100,
+    name: "Anzil",
+    email: "anzil049@gmail.com"
+};
+console.log(profileUser);
+let userSafe = {
+    id: 200,
+    name: "Anzil",
+    email: "anzil049@gmail.com",
+    isAdmin: true
+};
+console.log(userSafe);
--- utilityTypes/src/index.ts
+++ utilityTypes/src/index.ts
@@ -6,6 +6,9 @@ interface User{
     isAdmin:boolean;
 }
 
+
+//Partial
+
 type updateUser=Partial<User>;
 let userUpdate:updateUser={
     name:"Anzil",
@@ -13,3 +16,27 @@ let userUpdate:updateUser={
 }
 
 console.log(userUpdate);
+
+
+//Pick
+
+type userProfile=Pick<User,"id"|"name"|"email">
+let  profileUser:userProfile={
+    id:100,
+    name:"Anzil",
+    email:"anzil049@gmail.com"
+}
+
+console.log(profileUser);
+
+
+//Omit
+type safeUser=Omit<User,"password">
+let userSafe:safeUser={
+    id:200,
+    name:"Anzil",
+    email:"anzil049@gmail.com",
+    isAdmin:true,
+}
+
+console.log(userSafe);
